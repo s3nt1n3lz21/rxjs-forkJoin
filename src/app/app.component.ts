683daad4a42ad520b6fca0480e6e8593afbe5f2e
Neil Smith <s3nt1n3lz18@gmail.com>
2021-11-09 16:21:59 +0000

    Added example use of forkJoin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { forkJoin, of, interval } from 'rxjs';
+import { delay, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-forkJoin';
+
+  //Forkjoin is used for waiting for multiple observables/apis to complete before emitting all of that data at once. Its most commonly used for waiting for data to load when navigating to a page and that data comes from many different apis.
+  ngOnInit() {
+    const example = forkJoin({
+      observable1: of('Hello'),
+      observable2: of('World').pipe(delay(1000)),
+      observable3: interval(1000).pipe(take(1)),
+      observable4: interval(1000).pipe(take(2)),
+      observable5: of('RESULT')
+    })
+
+    example.subscribe(val => console.log(val))
+  }
 }
